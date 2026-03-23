fa1b244f4744bca6aa6c32701b3ba312e9a68af9
dldydtjr003 <dldydtjr002@gmail.com>
2026-03-23 11:43:23 +0900

    [수정] 파일이미지 나오게 수정

--- GgultemProject/src/main/java/com/honey/service/ItemBoardServiceImpl.java
+++ GgultemProject/src/main/java/com/honey/service/ItemBoardServiceImpl.java
@@ -39,9 +39,9 @@ public class ItemBoardServiceImpl implements ItemBoardService {
 	public ItemBoardDTO get(Long id) {
 		Optional<ItemBoard> result = itemBoardRepository.findById(id);
 		ItemBoard itemBoard = result.orElseThrow();
-		
-		itemBoard.chanceViewCount(itemBoard.getViewCount()+1);
-		
+
+		itemBoard.chanceViewCount(itemBoard.getViewCount() + 1);
+
 		itemBoardRepository.save(itemBoard);
 
 		ItemBoardDTO itemBoardDTO = modelMapper.map(itemBoard, ItemBoardDTO.class);
@@ -67,12 +67,11 @@ public class ItemBoardServiceImpl implements ItemBoardService {
 
 	private ItemBoard dtoToEntity(ItemBoardDTO itemBoardDTO) {
 		Member member = memberRepository.findById(itemBoardDTO.getEmail())
-	            .orElseThrow(() -> new IllegalArgumentException("DB에 없는 이메일입니다: " + itemBoardDTO.getEmail()));
-		
+				.orElseThrow(() -> new IllegalArgumentException("DB에 없는 이메일입니다: " + itemBoardDTO.getEmail()));
+
 		ItemBoard itemBoard = ItemBoard.builder().title(itemBoardDTO.getTitle()).writer(itemBoardDTO.getWriter())
 				.price(itemBoardDTO.getPrice()).content(itemBoardDTO.getContent()).category(itemBoardDTO.getCategory())
-				.location(itemBoardDTO.getLocation()).itemUrl(itemBoardDTO.getItemUrl())
-				.member(member)
+				.location(itemBoardDTO.getLocation()).itemUrl(itemBoardDTO.getItemUrl()).member(member)
 				.pictureUrl(itemBoardDTO.getPictureUrl()).enabled(0).status("판매중").build();
 		// 업로드 처리가 끝난 파일들의 이름 리스트
 		List<String> uploadFileNames = itemBoardDTO.getUploadFileNames();
@@ -88,21 +87,25 @@ public class ItemBoardServiceImpl implements ItemBoardService {
 
 	@Override
 	public PageResponseDTO<ItemBoardDTO> list(SearchDTO searchDTO) {
-		Pageable pageable = PageRequest.of(searchDTO.getPage() - 1, searchDTO.getSize(),
-				Sort.by("id").descending());
+		Pageable pageable = PageRequest.of(searchDTO.getPage() - 1, searchDTO.getSize(), Sort.by("id").descending());
 		Page<ItemBoard> result = null;
-		
-		if(searchDTO.getKeyword() != null && !searchDTO.getKeyword().isEmpty()) {
-			result = itemBoardRepository.searchByCondition(
-					searchDTO.getSearchType(),
-					searchDTO.getKeyword(),
-					pageable);
-		}else {
+
+		if (searchDTO.getKeyword() != null && !searchDTO.getKeyword().isEmpty()) {
+			result = itemBoardRepository.searchByCondition(searchDTO.getSearchType(), searchDTO.getKeyword(), pageable);
+		} else {
 			result = itemBoardRepository.findAllList(pageable);
 		}
-		
-		List<ItemBoardDTO> dtoList = result.getContent().stream()
-				.map(itemBoard -> modelMapper.map(itemBoard, ItemBoardDTO.class)).collect(Collectors.toList());
+
+		List<ItemBoardDTO> dtoList = result.getContent().stream().map(itemBoard -> {
+			ItemBoardDTO dto = modelMapper.map(itemBoard, ItemBoardDTO.class);
+
+			List<String> fileNameList = itemBoard.getItemList().stream().map(itemImage -> itemImage.getFileName())
+					.collect(Collectors.toList());
+
+			dto.setUploadFileNames(fileNameList);
+
+			return dto;
+		}).collect(Collectors.toList());
 		long totalCount = result.getTotalElements();
 		PageResponseDTO<ItemBoardDTO> responseDTO = PageResponseDTO.<ItemBoardDTO>withAll().dtoList(dtoList)
 				.pageRequestDTO(searchDTO).totalCount(totalCount).build();
@@ -116,24 +119,24 @@ public class ItemBoardServiceImpl implements ItemBoardService {
 		ItemBoard itemBoard = result.orElseThrow();
 
 		itemBoard.clearList();
-		
+
 		List<String> fileNames = itemBoardDTO.getUploadFileNames();
-		
+
 		// 3. 다시 하나씩 추가 (이 과정이 없으면 DB에서 삭제됩니다)
 		if (fileNames != null && !fileNames.isEmpty()) {
 			fileNames.forEach(name -> {
 				itemBoard.addImageString(name);
 			});
 		}
-		
+
 		itemBoard.changeTitle(itemBoardDTO.getTitle());
 		itemBoard.changePrice(itemBoardDTO.getPrice());
 		itemBoard.changeContent(itemBoardDTO.getContent());
 		itemBoard.changeCategory(itemBoardDTO.getCategory());
 		itemBoard.changeLocation(itemBoardDTO.getLocation());
-		
+
 		// 판매 상태(판매중, 판매완료)
-		if(itemBoardDTO.getStatus() != null && !itemBoardDTO.getStatus().isEmpty()) {
+		if (itemBoardDTO.getStatus() != null && !itemBoardDTO.getStatus().isEmpty()) {
 			itemBoard.changeStatus(itemBoardDTO.getStatus());
 		}
 
@@ -150,7 +153,7 @@ public class ItemBoardServiceImpl implements ItemBoardService {
 	public void remove(Long id) {
 		Optional<ItemBoard> result = itemBoardRepository.findById(id);
 		ItemBoard itemBoard = result.orElseThrow();
-		
+
 		itemBoard.changeEnabled(1);
 
 		itemBoardRepository.save(itemBoard);
